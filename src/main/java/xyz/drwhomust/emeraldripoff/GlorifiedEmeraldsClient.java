a3a8bd3f0dabe5f6f88ba58b8f8cfd4a930dbd3f
Drwhomust <rabbids8008@gmail.com>
2025-01-27 20:10:50 -0800

    Initial commit

---
+++ src/main/java/xyz/drwhomust/emeraldripoff/GlorifiedEmeraldsClient.java
@@ -0,0 +1,10 @@
+package xyz.drwhomust.emeraldripoff;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GlorifiedEmeraldsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}

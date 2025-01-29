668354b9471b14701f26483e596ebc782a17ab27
Drwhomust <rabbids8008@gmail.com>
2025-01-28 19:10:36 -0800

    added ruby

--- src/main/java/xyz/drwhomust/emeraldripoff/item/Moditems.java
+++ src/main/java/xyz/drwhomust/emeraldripoff/item/Moditems.java
@@ -1,6 +1,8 @@
 package xyz.drwhomust.emeraldripoff.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -16,6 +18,8 @@ public class Moditems {
     public static void registerModItems() {
         GlorifiedEmeralds.LOGGER.info("Registering Mod Items for " + GlorifiedEmeralds.MOD_ID);
 
-
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(ruby);
+        });
     }
 }

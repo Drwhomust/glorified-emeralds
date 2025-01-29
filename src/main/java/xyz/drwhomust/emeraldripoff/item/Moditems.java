6d634e16bb344f477f30524d84d0028384e25f66
Drwhomust <rabbids8008@gmail.com>
2025-01-28 18:48:04 -0800

    fixed name error

--- src/main/java/xyz/drwhomust/emeraldripoff/item/Moditems.java
+++ src/main/java/xyz/drwhomust/emeraldripoff/item/Moditems.java
@@ -6,15 +6,16 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import xyz.drwhomust.emeraldripoff.GlorifiedEmeralds;
 
-public static final Item ruby = registerItem(name:"ruby", new Item(new Item.Settings))
+public class Moditems {
+    public static final Item ruby = registerItem("ruby", new Item(new Item.Settings()));
 
-private static Item registerItem(String name, Item item) {
+    private static Item registerItem(String name, Item item) {
     return Registry.register(Registries.ITEM, Identifier.of(GlorifiedEmeralds.MOD_ID, name), item);
 }
 
-public class Moditems {
-
     public static void registerModItems() {
         GlorifiedEmeralds.LOGGER.info("Registering Mod Items for " + GlorifiedEmeralds.MOD_ID);
+
+
     }
 }

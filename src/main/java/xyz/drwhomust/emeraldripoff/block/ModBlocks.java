03abc5da89ad45ee01720879e439e8358a960c09
Drwhomust <rabbids8008@gmail.com>
2025-01-29 20:33:30 -0800

    added block files

--- src/main/java/xyz/drwhomust/emeraldripoff/block/ModBlocks.java
+++ src/main/java/xyz/drwhomust/emeraldripoff/block/ModBlocks.java
@@ -5,6 +5,7 @@ import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -17,7 +18,12 @@ import xyz.drwhomust.emeraldripoff.GlorifiedEmeralds;
 public class ModBlocks {
         public static final Block ruby_ore = registerBlock("ruby_ore",
                 new Block(AbstractBlock.Settings.create().registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(GlorifiedEmeralds.MOD_ID, "ruby_ore")))
-                        .strength(4f).requiresTool().sounds(BlockSoundGroup.STONE)));
+                        .strength(2f).sounds(BlockSoundGroup.STONE)));
+
+        public static final Block deepslate_ruby_ore = registerBlock("deepslate_ruby_ore",
+        new Block(AbstractBlock.Settings.create().registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(GlorifiedEmeralds.MOD_ID, "deepslate_ruby_ore")))
+                .strength(3f).sounds(BlockSoundGroup.DEEPSLATE)));
+
 
     private static Block registerBlock(String name, Block block) {
       registerBlockItem(name, block);
@@ -34,6 +40,7 @@ public class ModBlocks {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(fabricItemGroupEntries -> {
             fabricItemGroupEntries.add(ModBlocks.ruby_ore);
+            fabricItemGroupEntries.add(ModBlocks.deepslate_ruby_ore);
         });
     }
 }
